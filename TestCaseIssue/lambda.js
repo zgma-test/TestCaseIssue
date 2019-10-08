0f9f912f907d53d3310643e8dc6565f329182a3d
zgma-test <uditharosha+zgma@gmail.com>
2019-10-08 12:23:08 +0530

    2

--- TestCaseIssue/lambda.js
+++ TestCaseIssue/lambda.js
@@ -1,4 +1,4 @@
 exports.handler = function(event, context, callback) {
     
-    callback(null, {"message": "Successfully executed"});
+    callback(null, {"message": "Successfully executed1"});
 }
